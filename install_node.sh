621f256266855a2754475b4e373a6df5a488909b
thucycookie <tookdong@gmail.com>
2021-01-15 23:07:29 +0000

    Add a script to install node on Ubuntu

---
+++ install_node.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# ref: https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-18-04
+
+sudo apt update -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+nodejs -v
